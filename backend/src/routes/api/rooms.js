6a3a08ed5e9637521556a8384b463f60cfe0e6b6
Carson Ling <you@example.com>
2023-08-23 08:56:34 +0800

    adding posting data function

--- backend/src/routes/api/rooms.js
+++ backend/src/routes/api/rooms.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { retrieveRoomList } from '../../data/rooms-dao';
+import { retrieveRoomList, createRoom } from '../../data/rooms-dao';
 
 const HTTP_OK = 200; // Not really needed; this is the default if you don't set something else.
 const HTTP_CREATED = 201;
@@ -14,5 +14,16 @@ router.get('/', async(req, res) => {
     res.status(HTTP_OK)
         .json(roomsData);
 });
+
+// Create new article
+router.post('/', async (req, res) => {
+    const newRoom = await createRoom(req.body);
+
+    if (newRoom) return res.status(HTTP_CREATED)
+        .header('Location', `/api/rooms/${newRoom._id}`)
+        .json(newRoom);
+
+    return res.sendStatus(422);
+})
 
 export default router;
--- frontend/src/AllMeetingsPage.jsx
+++ frontend/src/AllMeetingsPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './AllMeetingsPage.module.css'
 import { Grid, Card, CardContent, CardActions, Typography, Button, Link } from '@mui/material';
+import axios from 'axios';
 // import { Button, Box, Grid, Drawer, IconButton, Slider, Typography, Switch, Select, MenuItem } from '@mui/material';
 // import { Link} from 'react-router-dom';
 // import SearchIcon from '@mui/icons-material/Search';
@@ -16,35 +17,47 @@ export default function AllMeetingsPage( {meetings} ){
             <p className={styles.title}> All Meetings</p>
             <div className={styles.meetingsContainer}>
                 <Grid container spacing={8} justifyContent="center" alignItems="center">
-                    {meetings.map((meeting) => (
-                        <Grid item key={meeting.name} xs={6.01} justifyContent="center" alignItems="center">
+                    {meetings.map((meeting, index) => (
+                        <Grid item key={index} xs={6.01}>
                             <Card>
-                                <CardContent>
-                                <CardActions>
-                                    <Button size="small" component={Link} to={`/club-meetings/${meeting._id}`}>Edit</Button>
-                                </CardActions>
-                                <Typography variant="h6">{meeting.club}</Typography>
-                                <Typography variant="body1">
-                                    Time: {meeting.startTime} - {meeting.endTime}
-                                </Typography>
-                                <Typography variant="body1">
-                                    Location: {meeting.location}
-                                </Typography>
+                                <CardContent className={styles.cardContent}>
+                                    <CardActions>
+                                        <Button size="small" component={Link} to={`/club-meetings/${meeting._id}`} className={styles.editButton}>Edit</Button>
+                                    </CardActions>
+                                    <Typography variant="h6" className={styles.clubTitle}>{meeting.club}</Typography>
+                                    <Typography variant="body1">
+                                        Time: {meeting.startTime} - {meeting.endTime}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        Location: {meeting.location}
+                                    </Typography>
+                                    <CardActions>
+                                        <Button size="small" component={Link} to={`/club-meetings/${meeting._id}`}>Cancel</Button>
+                                    </CardActions>
                                 </CardContent>
-                                <CardActions>
-                                    <Button size="small" component={Link} to={`/club-meetings/${meeting._id}`}>Cancel</Button>
-                                </CardActions>
                             </Card>
-                            {/* <div className={styles.meetingCard}> */}
-
-                            {/* </div> */}
                         </Grid>
+                         
+                        
                     ))}
                 </Grid>
+
+                <Button onClick={handleOnclick}>Add a Meeting</Button>
             </div>
             
         </div>
     );
+
+    async function handleOnclick(){
+        console.log('clicked')
+        const roomToUpload = {
+            name: 'New Room',
+            size: 'Big',
+            capacity: 100,
+            location: "SAC"
+        };
+        const roomsResponse = await axios.post('https://localhost:3000/api/rooms', roomToUpload);
+    }
 
 }
 
